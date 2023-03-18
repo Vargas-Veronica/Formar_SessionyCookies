146e5471e4d07327f4c698d4c0f65c9850e591bd
Vargas-Veronica <vargas.veronica55@gmail.com>
2023-03-18 14:57:03 -0300

    agrego estilos

---
+++ src/app.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+const session = require('express-session');
+const cookieParser = require("cookie-parser");
+const cookieCheck = require('./middlewares/cookieCheck');
+
+
+const port= process.env.PORT || 3000; 
+
+
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(cookieParser());
+app.use(session({
+    secret: "FormColor",
+    resave: false,
+    saveUninitialized: true
+}))
+app.use(cookieCheck)
+
+/* Template engine config */
+app.set('view engine', 'ejs');
+app.set('views', "./src/views");
+
+/* Routers */
+const mainRouter = require('./routes/main');
+/* Routers Middlewares */
+app.use('/', mainRouter);
+
+app.listen(port, () => {
+    console.log(`servidor levantado en http://localhost:${port}`);
+    }
+);
